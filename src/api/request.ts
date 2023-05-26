21b02c6a36be7f0fe6afa9f75090f5c3b18656f7
steveliu <liupeirong@ucla.edu>
2023-05-26 23:44:54 +0800

    feat: docs

--- src/api/request.ts
+++ src/api/request.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { proxy } from "../../.electron-config/config.json";
 
 export const baseInstance = axios.create({
-    timeout: 10000,
+    timeout: 60000,
     proxy: {
         host: proxy.host,
         port: proxy.port
